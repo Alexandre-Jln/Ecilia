9557745c792e9bc275b60068e5b32338968b34f4
Alexandre <alex4529@hotmail.fr>
2025-03-12 14:38:40 +0100

    Add render

--- render-build.sh
+++ render-build.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -eux
 
-# Dépendances PHP
+# Installer les dépendances PHP
 composer install --no-dev --optimize-autoloader
 
 # Installer les dépendances JS & builder les assets
 yarn install
 yarn encore production
 
-# Nettoie et mets en cache
+# Nettoyer et mettre en cache
 php bin/console cache:clear
 php bin/console cache:warmup
 
-# Exécute les migrations pour la base de données
+# Exécuter les migrations pour la base de données
 php bin/console doctrine:migrations:migrate --no-interaction
